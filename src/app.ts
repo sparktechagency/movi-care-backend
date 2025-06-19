c3cc4e7cf22889f47f6141b6eb2950687929ba0e
mdsharifulislam-r <masom3456@gmail.com>
2025-06-19 19:27:21 +0600

    add origin

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(Morgan.errorHandler);
 app.post('/api/webhook',express.raw({'type':'application/json'}),handleWebhook)
 //body parser
 app.use(cors({
-  origin:['http://localhost:3000','http://10.0.70.92:3000','http://10.0.70.92:5173','https://mithila.binarybards.online','https://e335-115-127-157-41.ngrok-free.app','http://31.97.171.35:4173'],
+  origin:['http://localhost:3000','http://10.0.70.92:3000','http://10.0.70.92:5173','https://mithila.binarybards.online','http://31.97.171.35:3000','http://31.97.171.35:4173'],
   credentials:true,
 }));
 app.use(express.json());
